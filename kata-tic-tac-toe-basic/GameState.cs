a4086d709e8d1b177a30b4bf05be62a0da497388
Liam Scott-Russell <liam.scott.russell@gmail.com>
2019-11-25 09:30:47 +1300

    Updates the GameState class to hold an array of all the players.

--- kata-tic-tac-toe-basic/GameState.cs
+++ kata-tic-tac-toe-basic/GameState.cs
@@ -4,11 +4,13 @@ namespace kata_tic_tac_toe_basic
     {
         Player CurrentPlayer { get; set; }
         Board CurrentBoard { get; set; }
+        Player[] AllPlayers { get; set; }
     }
 
     public class GameState : IGameState
     {
         public Board CurrentBoard { get; set; }
         public Player CurrentPlayer { get; set; }
+        public Player[] AllPlayers { get; set; }
     }
 }
