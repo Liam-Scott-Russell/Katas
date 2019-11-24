e0259465c3cc0e605c4dd422e2312c9161b3929b
Liam Scott-Russell <liam.scott.russell@gmail.com>
2019-11-24 13:04:49 +1300

    Adds most of the logic to the PlayGame() method.

--- kata-tic-tac-toe-basic/Program.cs
+++ kata-tic-tac-toe-basic/Program.cs
@@ -103,6 +103,11 @@ namespace kata_tic_tac_toe_basic
     class GameController
     {
         private GameState Game { get; set; } = new GameState();
+        private Dictionary<int, string> PlayerToSymbolMapping { get; set; } = new Dictionary<int, string>
+        {
+            {1, "X"},
+            {2, "O"}
+        };
 
         public GameController()
         {
@@ -114,17 +119,48 @@ namespace kata_tic_tac_toe_basic
         public void PlayGame()
         {
             PrintCurrentBoard();
-            Console.Out.WriteLine(WinConditions.IsGameDraw(Game));
+            while (true)
+            {
+                string userInput = GetInput();
+                if (userInput == "q")
+                {
+                    break;
+                }
+
+                string[] splitUserInput = userInput.Split(",");
+                PlayerMove move = new PlayerMove(new RawPlayerMove() 
+                    {XCoordinate = splitUserInput[0], YCoordinate = splitUserInput[1]}, Game);
+                
+                if (move.IsCoordinateValid() && move.IsCoordinateEmptyOnBoard())
+                {
+                    move.DrawOnBoard();
+                    
+                    switch (Game.CurrentPlayerTurn)
+                    {
+                        case 1:
+                            Game.CurrentPlayerTurn = 2;
+                            break;
+                        case 2:
+                            Game.CurrentPlayerTurn = 1;
+                            break;
+                    }
+                    Console.Clear();
+                    Console.Out.WriteLine("Move accepted!");
+                    PrintCurrentBoard();
+                }
+
+            }
         }
 
         public string GetInput()
         {
-            return "";
+            Console.Out.WriteLine($"Player {Game.CurrentPlayerTurn} enter a coord x,y to place your {PlayerToSymbolMapping[Game.CurrentPlayerTurn]} or enter 'q' to give up:");
+            return Console.ReadLine();
         }
 
         public void PrintCurrentBoard()
         {
-            Console.Out.WriteLine("Move accepted, here's the current board:");
+            Console.Out.WriteLine("Here's the current board:");
             int rowLength = Game.CurrentBoardState.GetLength(0);
             int colLength = Game.CurrentBoardState.GetLength(1);
 
