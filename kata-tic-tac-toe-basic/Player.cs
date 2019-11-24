6b607ec8f43a1994df7068fbfdde48ba26cec334
Liam Scott-Russell <liam.scott.russell@gmail.com>
2019-11-24 16:17:33 +1300

    Adds the Player class

---
+++ kata-tic-tac-toe-basic/Player.cs
@@ -0,0 +1,13 @@
+namespace kata_tic_tac_toe_basic
+{
+    interface IPlayer
+    {
+        string Symbol { get; set; }
+        int Number { get; set; }
+    }
+    public class Player : IPlayer
+    {
+        public string Symbol { get; set; }
+        public int Number { get; set; }
+    }
+}
