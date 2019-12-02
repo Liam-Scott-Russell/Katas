cf0b921ea9b37f3c078ab8c0a4a09981d5cff2ad
Liam Scott-Russell <liam.scott.russell@gmail.com>
2019-12-03 10:55:55 +1300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Payslip_Round_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
