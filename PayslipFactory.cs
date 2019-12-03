022ef1821ed2921990e7ef0ebf849a51d509d725
Liam Scott-Russell <liam.scott.russell@gmail.com>
2019-12-04 11:43:57 +1300

    Add ability for payslip factory to create payslips from a csv

--- PayslipFactory.cs
+++ PayslipFactory.cs
@@ -1,8 +1,36 @@
+using System;
+using System.Collections.Generic;
+
 namespace Payslip_Round_2
 {
     public class PayslipFactory
     {
         public static Payslip MakePayslip(Employee employee, PayPeriod payPeriod)
+        {
+            return CreatePayslipObject(employee, payPeriod);
+        }
+        
+        public static List<Payslip> MakePayslip(string csvFileName)
+        {
+            var allPayslips = new List<Payslip>();
+            var lines = CsvReaderWriter.ReadLines(csvFileName);
+
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var employee = CsvFormatter.ExtractEmployee(line);
+                employee.Country = Config.Australia; // TODO: Remove this hardcoded value once we have config input
+                
+                var payPeriod = CsvFormatter.ExtractPayPeriod(line);
+                var payslip = CreatePayslipObject(employee, payPeriod);
+                
+                allPayslips.Add(payslip);
+            }
+
+            return allPayslips;
+        }
+
+        private static Payslip CreatePayslipObject(Employee employee, PayPeriod payPeriod)
         {
             var calculator = new TaxCalculator(employee);
             return new Payslip()
@@ -17,11 +45,6 @@ namespace Payslip_Round_2
                     Super = calculator.GetSuper()
                 }
             };
-        }
-        
-        public static Payslip MakePayslip(string csvFileName)
-        {
-            return new Payslip();
         }
     }
 }
